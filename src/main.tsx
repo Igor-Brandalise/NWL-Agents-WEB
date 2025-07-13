409b1933679aae630864ad3ec0720d0467a60f79
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-12 21:29:12 -0300

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import './index.css'
 
-// biome-ignore lint/style/noNonNullAssertion: Mandatory by React
-createRoot(document.getElementById("root")!).render(
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+
+// biome-ignore lint/style/noNonNullAssertion: mandatory by React
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
