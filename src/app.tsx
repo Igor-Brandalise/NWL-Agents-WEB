781852c46a076a9e1f9dfd827dc34b4e2344166e
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-12 21:27:52 -0300

    import

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
 
-import './index.css'
+const queryClient = new QueryClient()
 
 export function App() {
-  
-
   return (
-   <div>
-
-   </div>
-
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
-
-
